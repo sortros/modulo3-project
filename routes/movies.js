27705cf095757867a8ab8afbfc62307949d2cce5
Josep <albiol957@hotmail.com>
2021-07-22 20:58:37 +0200

    wip

--- routes/movies.js
+++ routes/movies.js
@@ -1,86 +1,80 @@
-// const express = require('express');
-// const checkIfUserIsLoggedIn = require('../middlewares/auth');
-// const Recipe = require('../models/recipe-model');
-// const User = require('../models/user-model');
-// 
-// const router = express.Router();
-// 
-// router.get('/search', checkIfUserIsLoggedIn, (req, res) => {
-//   res.json;
-// });
-// 
-// router.post('/search', checkIfUserIsLoggedIn, (req, res, next) => {
-//   {
-//     res.json;
-//   }
-//   
-//     .then(recipesFounded => {
-//       res.json;
-//     })
-//     .catch(err => {
-//       next(err);
-//       res.json;
-//     });
-// });
-// 
-// router.get('/recipes/:id/details', checkIfUserIsLoggedIn, (req, res, next) => {
-//   Movie.findById(req.params.id)
-//     .then(movie => {
-//       res.json;
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// });
-// 
-// router.post('/recipes/:id/details', checkIfUserIsLoggedIn, (req, res, next) => {
-//   const user = req.session.currentUser;
-//   const { id } = req.params;
-//   // eslint-disable-next-line no-underscore-dangle
-//   User.findById(user._id)
-//     // eslint-disable-next-line consistent-return
-//     // eslint-disable-next-line no-shadow
-//     .then(user => {
-//       if (user.favouriteRecipes.includes(id) === true) {
-//         res.json;
-//         return;
-//       }
-//       user.favouriteRecipes.push(id);
-//       user.save();
-//       res.json;
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// });
-// 
-// router.get('/favourites', checkIfUserIsLoggedIn, (req, res, next) => {
-//   const user = req.session.currentUser;
-//   User.findById(user)
-//     .populate('favouriteRecipes')
-// 
-//     .then(userFounded => {
-//       res.json;
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// });
-// 
-// router.post('/favourites/:id', checkIfUserIsLoggedIn, (req, res, next) => {
-//   const user = req.session.currentUser;
-//   const { id } = req.params;
-//   // eslint-disable-next-line no-underscore-dangle
-//   User.findById(user._id)
-//     // eslint-disable-next-line no-shadow
-//     .then(user => {
-//       user.favouriteRecipes.splice(id, 1);
-//       return user.save();
-//     })
-//     .then(() => res.redirect('/favourites'))
-//     .catch(err => {
-//       next(err);
-//     });
-// });
+const express = require('express');
+const checkIfUserIsLoggedIn = require('../middlewares/auth');
+const Recipe = require('../models/recipe-model');
+const User = require('../models/user-model');
+
+const router = express.Router();
+router.get('/search', checkIfUserIsLoggedIn, (req, res) => {
+  res.json;
+});
+router.post('/search', checkIfUserIsLoggedIn, (req, res, next) => {
+  {
+    res.json;
+  }
+    .then(moviesFounded => {
+      res.json;
+    })
+    .catch(err => {
+      next(err);
+      res.json;
+    });
+});
+router.get('/moviess/:id/details', checkIfUserIsLoggedIn, (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.json;
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+router.post('/movies/:id/details', checkIfUserIsLoggedIn, (req, res, next) => {
+  const user = req.session.currentUser;
+  const { id } = req.params;
+  // eslint-disable-next-line no-underscore-dangle
+  User.findById(user._id)
+    // eslint-disable-next-line consistent-return
+    // eslint-disable-next-line no-shadow
+    .then(user => {
+      if (user.favouriteMovies.includes(id) === true) {
+        res.json;
+        return;
+      }
+      user.favouriteMovies.push(id);
+      user.save();
+      res.json;
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get('/favourites', checkIfUserIsLoggedIn, (req, res, next) => {
+  const user = req.session.currentUser;
+  User.findById(user)
+    .populate('favouriteMovies')
+    .then(userFounded => {
+      res.json;
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.post('/favourites/:id', checkIfUserIsLoggedIn, (req, res, next) => {
+  const user = req.session.currentUser;
+  const { id } = req.params;
+  // eslint-disable-next-line no-underscore-dangle
+  User.findById(user._id)
+    // eslint-disable-next-line no-shadow
+    .then(user => {
+      user.favouriteRecipes.splice(id, 1);
+      return user.save();
+    })
+    .then(() => res.json)
+    .catch(err => {
+      next(err);
+    });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,30 @@
-// const express = require('express');
-// const checkIfUserIsLoggedIn = require('../middlewares/auth');
-// const User = require('../models/user-model');
+const express = require('express');
+const checkIfUserIsLoggedIn = require('../middlewares/auth');
+const User = require('../models/user-model');
 
-// const router = express.Router();
-// 
-// router.get('/', (req, res) => res.json);
-// 
-// router.get('/help-page', (req, res) => res.json);
-// 
-// router.get('/welcome', checkIfUserIsLoggedIn, (req, res) => {
-//   res.json;
-// });
-// 
-// router.get('/profile/edit', checkIfUserIsLoggedIn, (req, res) => {
-//   res.json;
-// });
-// 
-// router.post('/profile/edit', checkIfUserIsLoggedIn, (req, res, next) => {
-//   const { _id } = req.session.currentUser;
-//   User.findByIdAndUpdate(_id, { username, email}, { new: true })
-//     .then(() => {
-//       res.json;
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// });
-// 
-// module.exports = router;
+const router = express.Router();
+
+router.get('/inicio', (req, res) => res.json);
+
+router.get('/seriesTV', (req, res) => res.json);
+
+router.get('/Peliculas', checkIfUserIsLoggedIn, (req, res) => {
+   res.json;
+});
+
+router.get('/profile/edit', checkIfUserIsLoggedIn, (req, res) => {
+ res.json;
+});
+
+router.post('/profile/edit', checkIfUserIsLoggedIn, (req, res, next) => {
+ const { _id } = req.session.currentUser;
+ User.findByIdAndUpdate(_id, { username, email}, { new: true })
+  .then(() => {
+  res.json;
+  })
+  .catch(err => {
+    next(err);
+  });
+});
+
+ module.exports = router;
